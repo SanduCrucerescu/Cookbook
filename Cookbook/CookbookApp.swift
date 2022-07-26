b822d6b16b6c9a31db6b4fead49fd96c3e28391e
Sandu Crucerescu <sc222tf@student.lnu.se>
2022-07-26 14:01:01 +0200

    Initial Commit

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Alex on 2022-07-26.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
