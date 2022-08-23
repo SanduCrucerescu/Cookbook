b5d8ea11a3bbeda3818413f53f8f66bfe2e87da5
Sandu Crucerescu <alexcrucerescu82@gmail.com>
2022-08-23 17:16:53 +0200

    Changes to the UserPageView

--- Cookbook/Views/UserPageView.swift
+++ Cookbook/Views/UserPageView.swift
@@ -60,112 +60,125 @@ struct UserCommentsSubMenu: View {
 }
 
 struct UserPageView: View {
-    @State var index: CGFloat = 0
+    @State var height: CGFloat = 0
     @EnvironmentObject var firebase: FirebaseViewModel
     
     var body: some View {
         GeometryReader { geo in
-            //ZStack {
                ScrollView(showsIndicators: false) {
                    Image("ThumbnailPhoto")
                         .resizable()
-                        .aspectRatio(CGSize(width: 1, height: 0.3),contentMode: .fit)
+                        .aspectRatio(CGSize(width: 1, height: 0.4),contentMode: .fit)
                        
                     // Contents
                    VStack {
-                       HStack {
-                           Image(uiImage: firebase.user.imageURL!)//systemName: "person.crop.circle")
-                            .resizable()
-                            .frame(width: 100, height: 100)
-                            //.clipShape(Circle())
-                            .overlay(Circle().strokeBorder(Color.backgroundColor, lineWidth: 7) )
-                            .padding(10)
-                           Spacer()
-                           Button {
-                               
-                           } label: {
-                               Text("Edit")
-                           }
-                           .padding(.vertical, 4)
-                           .padding(.horizontal)
-                           .background(
-                                Capsule()
-                                    .stroke(Color.sageGreen, lineWidth: 1.5))
-                           .padding(.top, 40)
-                           .padding(.trailing,20)
-                           
-
-                       }
-                       .padding(.top, -50)
+                       ContentsHeader()
                        
-                       VStack(alignment: .leading) {
-                            Spacer()
-                            Text(firebase.user.username)
-                                .font(.custom("ProximaNova-Regular",
-                                              size: 30))
-                                .foregroundColor(.textColor)
-                            Text(firebase.user.description)
-                                .font(.custom("ProximaNova-Regular",
-                                              size: 15))
-                                .foregroundColor(.textColor)
-                            HStack{
-                                Button {
-                                    
-                                } label: {
-                                    Text("Posts")
-                                        .font(.custom("ProximaNova-Regular",
-                                                      size: 20))
-                                        .foregroundColor(.textColor)
-                                }
-                                .frame(maxWidth: .infinity)
-                                
-                                Divider()
-                                    .frame(height: 30)
-                                
-                                Button {
-                                    print(index)
-                                } label: {
-                                    Text("Comments")
-                                        .font(.custom("ProximaNova-Regular",
-                                                      size: 20))
-                                        .foregroundColor(.textColor)
-                                }
-                                .frame(maxWidth: .infinity)
-                            }
-                            Divider()
-                            
-                            TabView {
-                                UserPostSubMenu(height: $index)
-                                UserCommentsSubMenu(height: $index)
-
-                            }
-                            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                            .frame(height: index)
-                        }
-                        .padding()
-                        
-                        .cornerRadius(10)
-                        .frame(maxHeight: .infinity, alignment: .bottom)
-                        .offset(y: -30.0)
+                       ContentsMain(height: $height)
                    }
-                  // .background(Color.green)
-                    
-                    
-//                        Image(systemName: "person.crop.circle")
-//                            .font(.system(size: 100))
-//                            .offset(y: -700.0)
-                    
                 }
-               .background(Color.backgroundColor)
-            //}
-            .navigationBarTitle(Text(""),
-                                displayMode: .inline)
-            .edgesIgnoringSafeArea(.vertical)
-            .onAppear {
-                index = geo.size.height
-            }
+                .background(Color.backgroundColor)
+                .navigationBarTitle(Text(""),
+                                    displayMode: .inline)
+                .edgesIgnoringSafeArea(.vertical)
+                .onAppear {
+                    height = geo.size.height
+                }
         }
     }
+    
+    struct ContentsHeader: View {
+        @EnvironmentObject var firebase: FirebaseViewModel
+        
+        var body: some View {
+            HStack {
+                Image(uiImage: firebase.user.imageURL!)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .overlay(Circle().strokeBorder(Color.backgroundColor, lineWidth: 7) )
+                    .padding(10)
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Edit")
+                }
+                .padding(.vertical, 4)
+                .padding(.horizontal)
+                .background(
+                    Capsule()
+                        .stroke(Color.sageGreen, lineWidth: 1.5))
+                .padding(.top, 40)
+                .padding(.trailing,20)
+                
+                
+            }
+            .padding(.top, -50)
+        }
+    }
+
+    struct ContentsMain: View {
+        @Binding var height: CGFloat
+        @EnvironmentObject var firebase: FirebaseViewModel
+        
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                Spacer()
+                Text(firebase.user.username)
+                    .font(.custom("ProximaNova-Regular",
+                                  size: 30))
+                    .foregroundColor(.textColor)
+                Text(firebase.user.description)
+                    .font(.custom("ProximaNova-Regular",
+                                  size: 15))
+                    .foregroundColor(.textColor)
+                HStack{
+                    Button {
+                        
+                    } label: {
+                        Text("Posts")
+                            .font(.custom("ProximaNova-Regular",
+                                          size: 20))
+                            .foregroundColor(.textColor)
+                    }
+                    .frame(maxWidth: .infinity)
+                    
+                    Divider()
+                        .frame(height: 30)
+                    
+                    Button {
+                        print(height)
+                    } label: {
+                        Text("Comments")
+                            .font(.custom("ProximaNova-Regular",
+                                          size: 20))
+                            .foregroundColor(.textColor)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                Divider()
+                
+                TabView {
+                    UserPostSubMenu(height: $height)
+                    UserCommentsSubMenu(height: $height)
+                    
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                .frame(height: height)
+            }
+            .padding()
+            
+            .cornerRadius(10)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            .offset(y: -30.0)
+        }
+    }
+
+    
+    
+    
 }
 
 
@@ -175,10 +188,10 @@ struct UserPageView: View {
 
 struct UserPageView_Previews: PreviewProvider {
     static var previews: some View {
-        let recipe = RecipeViewModel()
-        let firebase = FirebaseViewModel(recipeViewModel: recipe)
         UserPageView()
             //.environmentObject(firebase)
 
     }
 }
+
+
